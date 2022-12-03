7d625d15a6ce11a8e93fde4542cca8e6f58ac425
Kerem-1034003 <1034003@hr.nl>
2022-12-03 16:08:43 +0100

    database connect

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '3b6dbc5e2b88694100ea481536b72ff2'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    gebruikersnaam = db.Column(db.String(20), nullable=False)
+    wachtwoord = db.Column(db.String(80), nullable=False)
 
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/login")
 def login():
     return render_template('login.html')
 
-@app.route("/register")
-def register():
-    return render_template('register.html')
+
+@app.route("/registreer")
+def registreer():
+    return render_template('registreer.html')
